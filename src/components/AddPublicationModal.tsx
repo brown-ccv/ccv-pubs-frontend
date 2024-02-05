5b82333c91847edfd31445a771e82b20035c5460
Ellen Duong <shi.ellenduong@gmail.com>
2024-02-05 14:53:56 -0500

    feat: add formik to manual form

--- src/components/AddPublicationModal.tsx
+++ src/components/AddPublicationModal.tsx
@@ -6,7 +6,8 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import { extractDOI, fetchDoi } from '../utils/utils.ts';
 
-const formID = 'form';
+const searchFormId = 'searchForm';
+const manualFormId = 'manualForm';
 
 export function AddPublicationModal() {
   const [show, setShow] = useState(false);
@@ -65,7 +66,7 @@ const SearchDoiForm = ({ setManual }: { setManual: (manual: boolean) => void }) 
           }}
         >
           {({ handleChange, handleSubmit, values, errors }) => (
-            <Form id={formID} onSubmit={handleSubmit}>
+            <Form id={searchFormId} onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="validationDoiOrUrl">
                 <Form.Label>DOI or DOI URL</Form.Label>
                 <Form.Control
@@ -87,7 +88,7 @@ const SearchDoiForm = ({ setManual }: { setManual: (manual: boolean) => void }) 
         <Button type="button" variant="contained" color="secondary" onClick={() => setManual(true)}>
           Enter Manually
         </Button>
-        <Button type="submit" variant="contained" color="primary" form={formID}>
+        <Button type="submit" variant="contained" color="primary" form={searchFormId}>
           Search
         </Button>
       </Modal.Footer>
@@ -105,45 +106,115 @@ const ManualForm = ({
   return (
     <>
       <Modal.Body>
-        <Form.Group className="mb-3" controlId="title">
-          <Form.Label>Title</Form.Label>
-          <Form.Control type="text" placeholder="Enter title" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="authors">
-          <Form.Label>Author(s)</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter Author(s) names (ex. John Smith, Jane Doe, ...)"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="publisher">
-          <Form.Label>Publisher</Form.Label>
-          <Form.Control type="text" placeholder="Enter Publisher" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="authors">
-          <Form.Label>Volume</Form.Label>
-          <Form.Control type="text" placeholder="Enter Volume" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="authors">
-          <Form.Label>URL</Form.Label>
-          <Form.Control type="text" placeholder="Enter URL" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="DOI">
-          <Form.Label>DOI</Form.Label>
-          <Form.Control type="text" placeholder="Enter DOI" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="month">
-          <Form.Label>Month of Publication</Form.Label>
-          <Form.Control type="number" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="year">
-          <Form.Label>Year of Publication</Form.Label>
-          <Form.Control type="number" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="abstract">
-          <Form.Label>Abstract</Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="Enter Abstract" />
-        </Form.Group>
+        <Formik
+          initialValues={{
+            title: '',
+            authors: '',
+            publisher: '',
+            url: '',
+            doi: '',
+            month: 0,
+            year: 0,
+            abstract: '',
+          }}
+          onSubmit={(values) => {
+            console.log(values);
+            handleClose();
+          }}
+        >
+          {({ handleChange, handleSubmit, values, errors }) => (
+            <Form id={manualFormId} onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="title">
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="title"
+                  placeholder="Enter title"
+                  value={values.title}
+                  onChange={handleChange}
+                  isInvalid={!!errors.title}
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="authors">
+                <Form.Label>Author(s)</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="authors"
+                  placeholder="Enter Author(s) names (ex. John Smith, Jane Doe, ...)"
+                  value={values.authors}
+                  onChange={handleChange}
+                  isInvalid={!!errors.authors}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="publisher">
+                <Form.Label>Publisher</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="publisher"
+                  placeholder="Enter Publisher"
+                  value={values.publisher}
+                  onChange={handleChange}
+                  isInvalid={!!errors.publisher}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="url">
+                <Form.Label>URL</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="url"
+                  placeholder="Enter URL"
+                  value={values.url}
+                  onChange={handleChange}
+                  isInvalid={!!errors.url}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="doi">
+                <Form.Label>DOI</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="doi"
+                  placeholder="Enter DOI"
+                  value={values.doi}
+                  onChange={handleChange}
+                  isInvalid={!!errors.doi}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="month">
+                <Form.Label>Month of Publication</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="month"
+                  value={values.month}
+                  onChange={handleChange}
+                  isInvalid={!!errors.month}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="year">
+                <Form.Label>Year of Publication</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="year"
+                  value={values.year}
+                  onChange={handleChange}
+                  isInvalid={!!errors.year}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="abstract">
+                <Form.Label>Abstract</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  name="abstract"
+                  placeholder="Enter Abstract"
+                  value={values.abstract}
+                  onChange={handleChange}
+                  isInvalid={!!errors.abstract}
+                />
+              </Form.Group>
+            </Form>
+          )}
+        </Formik>
       </Modal.Body>
       <Modal.Footer>
         <Button
@@ -154,7 +225,7 @@ const ManualForm = ({
         >
           Return to search
         </Button>
-        <Button type="submit" variant="contained" color="primary" form={formID}>
+        <Button type="submit" variant="contained" color="primary" form={manualFormId}>
           Submit
         </Button>
       </Modal.Footer>
