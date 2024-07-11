216febe95ddbd770cbaa7e36e404da1cfa738116
Anna Murphy <anna_murphy1@brown.edu>
2024-07-11 15:34:48 -0400

    feat: new script to refactor old pub docs

---
+++ scripts/addQueryTokensToPubs.ts
@@ -0,0 +1,58 @@
+import { existsSync } from 'fs';
+
+import admin from 'firebase-admin';
+import { applicationDefault } from 'firebase-admin/app';
+import { Timestamp } from 'firebase-admin/firestore';
+
+import { createPublicationTokens } from '../src/utils/firebase';
+
+const SERVICE_ACCOUNT_PATH = './scripts/serviceAccount.json';
+
+type DB = ReturnType<(typeof admin)['firestore']>;
+type Pubs = Awaited<ReturnType<typeof getPubs>>;
+
+function setup() {
+  if (existsSync(SERVICE_ACCOUNT_PATH)) {
+    process.env.GOOGLE_APPLICATION_CREDENTIALS = SERVICE_ACCOUNT_PATH;
+    console.log('Using Service Account Json');
+  } else {
+    console.log(
+      'Unable to find Service Account Json. Script will fail to write to the remote database.'
+    );
+  }
+
+  admin.initializeApp({ credential: applicationDefault() });
+  const db = admin.firestore();
+  return { admin, db };
+}
+
+async function getPubs(db: DB) {
+  const moduleDocs = await db.collection('publications').get();
+  if (moduleDocs.empty) {
+    console.error('No publications found.');
+    return [];
+  }
+  return moduleDocs.docs;
+}
+
+async function updatePubsWithTokens(db: DB, pubs: Pubs) {
+  await Promise.all(
+    pubs.map((pub) => {
+      const pubData = pub.data() as { title: string; author: string; updatedAt: number };
+      return db.doc(`publications/${pub.id}`).set({
+        ...pubData,
+        updatedAt: Timestamp.fromDate(new Date(pubData.updatedAt)),
+        tokens: createPublicationTokens(pubData),
+      });
+    })
+  );
+}
+
+async function main() {
+  const { db } = setup();
+  const pubs = await getPubs(db);
+  await updatePubsWithTokens(db, pubs);
+  console.log('Done.');
+}
+
+main();
